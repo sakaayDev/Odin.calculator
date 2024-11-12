5f501302ee772bdd486ead88b2ee689be51ab404
sakaayDev <sakaayhakim02@gmail.com>
2024-11-12 05:14:54 +0100

    Add backspace  and limit "." to 1 per number

--- calculator.js
+++ calculator.js
@@ -18,7 +18,7 @@ function power(number1,number2){
     return number1 ** number2;
 };
 // init variable that hold number1,number2,operator,result
-let numbers,result,resultBtn,clear,operators,number1,number2,operat;
+let numbers,decimalP,result,resultBtn,clear,operators,number1,number2,operat,backspaceBtn;
 
 //function that return whatever math operation we want depending on operator chosen 
 function operate(number1,operator,number2){
@@ -26,7 +26,7 @@ function operate(number1,operator,number2){
         return add(number1,number2)
     }else if(operator ==="-"){
         return substract(number1,number2)
-    }else if(operator ==="/"){
+    }else if(operator ==="÷"){
         return divide(number1,number2)        
     }else if(operator ==="*"){
         return multiply(number1,number2)
@@ -37,13 +37,38 @@ function operate(number1,operator,number2){
 
 let display = document.querySelector(".mainDisplay");
 let secondaryDisplay = document.querySelector(".secondaryDisplay");
-display.value =0;
+
 //handle numbers click,also save number2 into it's variable
+decimalP = document.querySelector(".decimalpoint");
+decimalP.addEventListener("click",()=>{
+    if(display.value ==="ERROR"){
+        clearCalculator();
+        decimalReset();
+    // }else if(display.vlaue ===null || display.value ===""){
+    //     decimalReset();
+    // }else if(display.value ===operat){
+    //     display.value =0;
+    //     display.value +=decimalP.textContent;
+    //     secondaryDisplay +=decimalP.textContent;
+    // }
+    }else if(display.value.includes(".")){
+
+    }else{
+        display.value +=decimalP.textContent;
+        secondaryDisplay.value +=decimalP.textContent
+    }
+    
+
+});
+
 numbers = document.querySelectorAll(".number");
 let numberClicked =numbers.forEach((number)=>{
     number.addEventListener("click",()=>{
+        if(display.value ==="ERROR"){
+            clearCalculator();
+        };
         if(display.value === operat){
-            display.value="";
+            display.value=null;
             display.value += number.textContent;
             number2 = Number(display.value);
         }else if(operat){
@@ -59,48 +84,80 @@ let numberClicked =numbers.forEach((number)=>{
 operators = document.querySelectorAll(".operator");
 operators.forEach((operator)=>{
     operator.addEventListener("click",()=>{
+        if(display.value ==="ERROR"){
+            clearCalculator();
+        };
         if(number1 && result){
             number1 = result;
-            display.value="";
+            display.value=null;
             display.value += operator.textContent;
             operat = display.value;
         }
         else if(number1){
-            display.value="";
+            display.value=null;
             display.value += operator.textContent;
             operat = display.value;
         }
-        else if(display.value !="" ){
+        else if(display.value !=null ){
             number1 = Number(display.value);
-            display.value="";
+            display.value=null;
             display.value += operator.textContent;
             operat = display.value;
         };
         secondaryDisplay.value += operator.textContent;
     });
 });
-
+//clear the display by removing everything
 clear = document.querySelector("#clear");
 clear.addEventListener("click",()=>{
-    display.value ="";
-    secondaryDisplay.value="";
-    number1="";
-    number2="";
-    operat="";
-    result="";
     
+    clearCalculator();
 });
 
+backspaceBtn = document.querySelector("#backspace");
+backspaceBtn.addEventListener("click", () => {
+    // Remove the last character from the display's value
+    if(Number(display.value) === result){
+        let slicedResult = result.toString().slice(0,-1);
+        result = Number(slicedResult);
+    }else if (Number(display.value) === number2){
+        let slicednumber2 = number2.toString().slice(0,-1);
+        number2 = Number(slicednumber2);
+    };
+    display.value = display.value.slice(0, -1);
+    secondaryDisplay.value = secondaryDisplay.value.slice(0,-1);
+});
+
+//what happen when click = btn 
 resultBtn = document.querySelector("#resultBtn");
 resultBtn.addEventListener("click",()=>{
     ;
     if(number1,number2,operat){
-        display.value="";
+        if(number2 ===0 && operat ==="÷"){
+            display.value ="ERROR";
+            secondaryDisplay.value="ERROR";
+        }else
+        {display.value=null;
         result = operate(number1,operat,number2);
         display.value = result;
-    }
-})
+        secondaryDisplay.value = result;}
+        
+    };
+});
 
 
-
-
+function clearCalculator() {
+    display.value =null;
+    secondaryDisplay.value=null;
+    number1=null;
+    number2=null;
+    operat=null;
+    result=null;
+    
+};
+function decimalReset(){
+    display.value =0;
+    secondaryDisplay=0;
+    display.value +=decimalP.textContent;
+    secondaryDisplay +=decimalP.textContent;
+};
